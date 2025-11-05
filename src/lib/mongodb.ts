9c79da9e7cfa51bcddafdb7f32851e5383655625
Sarpanchshab <monu1999june@gmail.com>
2025-11-05 14:36:50 +0530

    mongodb problem fix

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -7,7 +7,7 @@ export const connectDB = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI as string, {
-      dbName: 'tedx-admin',
+      dbName: 'tedxofficial',
     });
     isConnected = true;
     console.log('✅ MongoDB connected');
